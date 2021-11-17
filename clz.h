88240dae237eceefa18703f6561c024b1f0f5395
RuiChenZhai <54008162+Malones314@users.noreply.github.com>
2021-11-17 13:40:13 +0800

    Update clz.h

--- clz.h
+++ clz.h
@@ -1,25 +1,25 @@
 /**二分法算前导零
   **************/
 int clz ( uint32_t x ) {
-	int n = 0;
-	if ( x <= 0x0000ffff ) {
-		n += 16;
-		x <<= 16;
-	}
-	if ( x <= 0x00ffffff ) {
-		n += 8;
-		x <<= 8;
-	}
-	if ( x <= 0x0fffffff ) {
-		n += 4;
-		x <<= 4;
-	}
-	if ( x <= 0x3fffffff ) {
-		n += 2;
-		x <<= 2;
-	}
-	if ( x <= 0x7fffffff ) {
-		n++;
-	}
-	return n;
+    int n = 0;
+    if ( x <= 0x0000ffff ) {
+	n += 16;
+	x <<= 16;
+    }
+    if ( x <= 0x00ffffff ) {
+	n += 8;
+	x <<= 8;
+    }
+    if ( x <= 0x0fffffff ) {
+	n += 4;
+	x <<= 4;
+    }
+    if ( x <= 0x3fffffff ) {
+	n += 2;
+	x <<= 2;
+    }
+    if ( x <= 0x7fffffff ) {
+	n++;
+    }
+    return n;
 }
